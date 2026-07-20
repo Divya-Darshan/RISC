73ce3086e0fa52e005ee803a3a7ec425531b97d1
Divya Darshan <parukrish.darshan20@gmail.com>
2026-07-20 16:21:34 +0100

    I can find a way to print number IDK😣

--- riscv.asm
+++ riscv.asm
@@ -1,9 +1,10 @@
 .text
 main:
-    li t0, 5
-    li t1, 10
-    
-    add t2, t0, t1
-    
-    li a7, 10       # MAKE SURE THIS LINE SAYS 'a7', NOT ANYTHING ELSE!
-    ecall
+li t0, 5
+li t1, 10
+add t2, t0, t1
+li a7, 1
+mv a0, t2
+ecall
+li a7, 10
+ecall
